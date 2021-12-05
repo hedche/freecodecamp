0ac8ac1c8d54d123a7999e2eab767324c9e00462
hedche <montyrhodes8@gmail.com>
2021-12-05 18:49:16 +0000

    its a start

--- boilerplate-polygon-area-calculator-master/shape_calculator.py
+++ boilerplate-polygon-area-calculator-master/shape_calculator.py
@@ -1,6 +1,14 @@
 class Rectangle:
+    def __init__(self, width, height):
+        set_width = self.width
+        set_height = self.height
 
+    def get_area(self):
+        return self.width * self.height 
 
+    def get_perimeter(self):
+        return (2 * self.width + 2 * self.height)
 
-
-class Square:
+class Square(Rectangle):
+    def __init__(self):
+        square_height = 
