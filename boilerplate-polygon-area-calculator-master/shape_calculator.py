434bba2947159480efc60540476cd9107574c535
hedche <montyrhodes8@gmail.com>
2021-12-07 16:19:20 +0000

    Start on the polygon project

--- boilerplate-polygon-area-calculator-master/shape_calculator.py
+++ boilerplate-polygon-area-calculator-master/shape_calculator.py
@@ -1,14 +1,28 @@
 class Rectangle:
     def __init__(self, width, height):
-        set_width = self.width
-        set_height = self.height
+        self.width = width
+        self.heigth = height
 
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+    
     def get_area(self):
         return self.width * self.height 
 
     def get_perimeter(self):
         return (2 * self.width + 2 * self.height)
 
+    def get_diagonal(self):
+        return ((self.width ** 2 + self.height ** 2) ** .5)
+
+    def get_picture(self):
+        if (self.width > 50 or self.height > 50):
+            return "Too big for picture."
+
 class Square(Rectangle):
-    def __init__(self):
-        square_height = 
+    def __init__(self, side):
+        self.width = side
+        self.height = side
