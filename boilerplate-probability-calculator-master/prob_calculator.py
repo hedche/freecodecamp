d6da9dc87407c88d115c01f1f43f895d88fb8c09
hedche <montyrhodes8@gmail.com>
2021-12-07 18:08:00 +0000

    added innit method

--- boilerplate-probability-calculator-master/prob_calculator.py
+++ boilerplate-probability-calculator-master/prob_calculator.py
@@ -2,7 +2,9 @@ import copy
 import random
 # Consider using the modules imported above.
 
-class Hat:
+class Hat:#
+    def __init__(self) -> None:
+        pass
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
