d96b3c764e7cdd22994b8ae6326550a4a7649ea6
hedche <montyrhodes8@gmail.com>
2021-12-11 13:45:22 +0000

    need a list for dow_dict

--- boilerplate-time-calculator-master/time_calculator.py
+++ boilerplate-time-calculator-master/time_calculator.py
@@ -1,6 +1,8 @@
 def add_time(start, duration, dow="none"):
 	
-	week_dict = { 
+	days = 0
+	new_dow = " "
+	dow_dict = { 
 		"monday": 0, 
 		"tuesday": 1, 
 		"wednesday": 2, 
@@ -9,11 +11,14 @@ def add_time(start, duration, dow="none"):
 		"saturday": 5, 
 		"sunday": 6 
 		}
-
+	value = 3
+	if value in dow_dict:
+		print(dow_dict[value])
 	#If there was no Day of the Week (dow) passed as a param
-	if dow not in week_dict :
-                lower_dow = dow.lower()
-                number_dow = week_dict[lower_dow]
+	if dow != "none" :
+		lower_dow = dow.lower()
+
+	number_dow = dow_dict[lower_dow]
 	
 	#Going from "1:20 PM" to ("1:20", "PM")	
 	split_start = start.split()
@@ -25,16 +30,46 @@ def add_time(start, duration, dow="none"):
 	start_minute = int(split_hm[1])
 	
 	duration_hours = int((duration.split(":")[0]))
-	
+	duration_minutes = int((duration.split(":")[1]))
+
 	#If it's PM add on 12hrs to the hour of split_hm[0]
 	if split_start[1] == "PM":
-		
 		total_hours = start_hour + 12
 
 	total_hours = start_hour + duration_hours
+	total_minutes = start_minute + duration_minutes
+
+	#Calculates days based on a 24hr day
+	while total_hours > 24:
+		days += 1
+		total_hours -= 24
+				
+	dow_total = number_dow + days
+
+	if days > 1:
+		days_later = f"({days} days later)"
 		
+		#If days elapsed are over a week, carry over for wee
+		while dow_total > 6:
+			dow_total = dow_total - 7
+		print(type(dow_total))
+		
+		new_dow = dow_dict[dow_total]
 
-	return total_hours
+	#Formats the 24Hr time to 12Hr with AM and PM
+	if total_hours < 24 and total_hours > 12:
+		total_hours -= 12
+		AorP = " PM"
+	else:
+		AorP = " AM"
+
+	#Calculates if days elapsed are over a week
+	while dow_total > 6:
+		dow_total = dow_total - 7
+
+	new_time = str(total_hours) + ":" + str(total_minutes) + AorP + new_dow + days_later
+
+	return new_time
 
 
-print(add_time("1:20 PM", "24:20", "tueSday"))
+print(add_time("1:20 PM", "230:20", "tueSday"))
