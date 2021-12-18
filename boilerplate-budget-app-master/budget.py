0994635047524ba5a892e6d9c480a0e8daa2dc61
hedche <montyrhodes8@gmail.com>
2021-12-18 16:22:44 +0000

    __str__ method

--- boilerplate-budget-app-master/budget.py
+++ boilerplate-budget-app-master/budget.py
@@ -4,25 +4,47 @@ class Category:
         self.name = name
         self.ledger = list()
 
+    def __str__(self) -> str:
+        title_length = 30
+        
+
     #Deposit method accepts amount and description (optional) then appends object to the ledger list
     def deposit(self, amount, description=""):
-        
         self.ledger.append({"amount": amount, "description": description})
 
     #Withdraw method is similar to deposit, however, it will be a negative number in the ledger
     def withdraw(self, amount, description=""):
         
-        #If there is still funds, append.
+        #If there is still funds, append and return True, if not return False
         if(self.check_funds(amount)):
-            
             self.ledger.append({"amount": -amount, "description": description})
             return True;
+        return False
+
+    #This will add all the appended amounts of the ledger list
+    def get_balance(self):
+        total = 0
+        for item in self.ledger:
+            total += item["amount"]
+        return total
+        
+    #Transfer method which will transfer amounts using deposit and withdraw methods  
+    def transfer(self, amount, category):
+        
+        #If there is still funds, append.
+        if(self.check_funds(amount)):
+            category.deposit(amount, "Transfer from " + self.name)
+            self.withdraw(amount,"Transfer to " + category.name)
+            return True
         
         return False
 
-    def get_balance(self, amount):
-        
-        total = sum(self.ledger)
+    def check_funds(self, amount):
+        if(self.get_balance() >= amount):
+            return True
+        return False
 
 def create_spend_chart(categories):
+    astrk = "*"
+    
     return None
