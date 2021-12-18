78a82a4c84a32ae200e323f7c1335f31df240043
hedche <montyrhodes8@gmail.com>
2021-12-18 15:43:30 +0000

    WIP

--- boilerplate-budget-app-master/budget.py
+++ boilerplate-budget-app-master/budget.py
@@ -1,7 +1,19 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = list()
 
+    #Deposit method accepts amount and description (optional) then appends object to the ledger list
+    def deposit(self, amount, description=""):
+        
+        self.ledger.append({"amount": amount, "description": description})
 
+    #With
+    def withdraw(self, amount, description=""):
+        
+        self.ledger.append({"amount": amount, "description": description})
 
 
 
 def create_spend_chart(categories):
+    return None
