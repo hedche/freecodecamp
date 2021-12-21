94a9f3da63d1e2330f5fbadc5f8959ea1ae42377
hedche <montyrhodes8@gmail.com>
2021-12-21 18:32:38 +0000

    finishing up experiment function

--- boilerplate-probability-calculator-master/prob_calculator.py
+++ boilerplate-probability-calculator-master/prob_calculator.py
@@ -3,7 +3,7 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    #kwargs will be our disctionary for all the parameters passed in
+    #kwargs will be our dictionary for all the parameters passed in
     def __init__(self, **kwargs):
         self.contents = []
         for key, value in kwargs.items():
@@ -12,13 +12,15 @@ class Hat:
         #print(self.contents)
 
     def draw(self, number):
+        
         all_removed = []
-        # If the number of balls to draw exceeds the avaliable quantitiy, return all the balls.
+        
+        #If the number of balls to draw exceeds the avaliable quantitiy, return all the balls.
         if(number > len(self.contents)):
             return self.contents
-        
-        # This will remove a ball at random from self.contents list using pop()
+        #This will loop for the amount specified by number
         for i in range(number):
+            # This will remove a ball at random from self.contents list using pop()
             removed = self.contents.pop(int(random.random() * len(self.contents)))
             all_removed.append(removed)
         return all_removed
@@ -26,4 +28,17 @@ class Hat:
     
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for i in range(num_experiments):
+        #Using the ipmorted copy library for "freezing" the expected_balls dict
+        expected_balls_copy = copy.deepcopy(expected_balls)
+        
+        #Same deep copy here so that hat_copy does not change with hat
+        hat_copy = copy.deepcopy(hat)
+        colours_gotten = hat_copy.draw(num_balls_drawn)
+        
+        for colours in colours_gotten:
+            if(colour in expected_balls_copy):
+                expected_balls_copy[colour]-=1
+        
     return None
