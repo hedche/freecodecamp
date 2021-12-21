346da96af641ebd17951aa297a2fff5fa5b1e1af
hedche <montyrhodes8@gmail.com>
2021-12-21 16:40:07 +0000

    start on prob calc

--- boilerplate-probability-calculator-master/prob_calculator.py
+++ boilerplate-probability-calculator-master/prob_calculator.py
@@ -2,9 +2,18 @@ import copy
 import random
 # Consider using the modules imported above.
 
-class Hat:#
-    def __init__(self) -> None:
-        pass
+class Hat:
+    #kwargs will be our disctionary for all the parameters passed in
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
+                
+        print(self.contents)
 
+    def draw(self, number):
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    return None
