d898b9e353069c42efae2445f3ec516046447825
hedche <montyrhodes8@gmail.com>
2021-12-21 17:41:12 +0000

    finished draw method

--- boilerplate-probability-calculator-master/prob_calculator.py
+++ boilerplate-probability-calculator-master/prob_calculator.py
@@ -8,12 +8,22 @@ class Hat:
         self.contents = []
         for key, value in kwargs.items():
             for i in range(value):
-                self.contents.append(key)
-                
-        print(self.contents)
+                self.contents.append(key)    
+        #print(self.contents)
 
     def draw(self, number):
+        all_removed = []
+        # If the number of balls to draw exceeds the avaliable quantitiy, return all the balls.
+        if(number > len(self.contents)):
+            return self.contents
         
+        # This will remove a ball at random from self.contents list using pop()
+        for i in range(number):
+            removed = self.contents.pop(int(random.random() * len(self.contents)))
+            all_removed.append(removed)
+        return all_removed
+    
+    
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     return None
